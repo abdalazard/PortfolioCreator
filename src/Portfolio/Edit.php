e37ca62b7e25e18669bd1d0cde54c11a98516369
Abdalazard <abdalazard@gmail.com>
2023-08-26 05:30:23 -0300

    Cria queries e seta dados ao banco

--- src/Portfolio/Edit.php
+++ src/Portfolio/Edit.php
@@ -1,6 +1,13 @@
 <?php
+include '../../auth/Authentication.php';
+include '../../db/Connection.php';
 
 class Edit {
+
+    //User
+    public $userId;
+    //Database
+    public $db;
 
     //Profile
     public $imagem;
@@ -21,33 +28,47 @@ class Edit {
     public $github;
     public $linkedin;
 
+    public function __construct(){
+        $this->db = new Connection;
+        $this->userId = $_SESSION['id'];
+    }
+
     public function setProfile($imagem, $titulo, $subtitulo) {
         //Profile   
         $this->imagem = $imagem;
         $this->titulo = $titulo;
         $this->subtitulo = $subtitulo;
-        
+        $query = '';
+        $db->toDatabase($query);
     }   
     
     public function setProjects($nome_projeto, $url) {
         //Project
         $this->nome_projeto = $nome_projeto;
         $this->link_projeto = $url;
+        $query = '';
+        $db->toDatabase($query);
     }
 
     public function setSkill($skill) {
         //Skills
         $this->skills = $skill;
+        $query = '';
+        $db->toDatabase($query);
     }
 
     public function setOthers($others) {
         //Others
         $this->others = $others;
+        $query = '';
+        $db->toDatabase($query);
     }
 
     public function setSocial($github, $linkedin){
         //Social
         $this->github = $github;
         $this->linkedin = $linkedin;
+        $query = '';
+        $db->toDatabase($query);
     }
 }
