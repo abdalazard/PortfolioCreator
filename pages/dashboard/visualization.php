41849145daa2d1a4aafe2d3533f3a10de1077b04
ABDALAZARD <abdalazard@gmail.com>
2023-08-28 22:23:42 -0300

    corrige estilo

--- pages/dashboard/visualization.php
+++ pages/dashboard/visualization.php
@@ -41,7 +41,9 @@ include_once '../../icon/network.php';
     <meta property="og:url" content="https://abdalazard.online" />
     <meta property="twitter:card" content="summary_large_image" />
     <link rel="canonical" href="https://abdalazard.online" />
-    <link href="https://fonts.googleapis.com/css2?display=swap&family=Inter:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet" type="text/css" />
+    <link
+        href="https://fonts.googleapis.com/css2?display=swap&family=Inter:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600"
+        rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="../../styles.css" />
     <link type="text/css" rel="stylesheet" href="../../materialize/css/materialize.min.css" media="screen,projection" />
     <script type="text/javascript" src="../../index.js">
@@ -66,7 +68,7 @@ include_once '../../icon/network.php';
                 </li>
             </ul>
             <ul class="right">
-                <li><a class="waves-effect waves-light btn black modal-trigger " href="#">>Voltar</a>
+                <li><a class="waves-effect waves-light btn black modal-trigger " href="#">Voltar</a>
                 </li>
             </ul>
 
@@ -78,7 +80,8 @@ include_once '../../icon/network.php';
                 <div id="container01" class="style1 container default">
                     <div class="wrapper">
                         <div class="inner" data-onvisible-trigger="1">
-                            <div id="image04" class="style1 image"><span class="frame"><img src="<?php echo '../../' . $info['path']; ?>" alt="foto" /></span></div>
+                            <div id="image04" class="style1 image"><span class="frame"><img
+                                        src="<?php echo '../../' . $info['path']; ?>" alt="foto" /></span></div>
                             <h2 id="text05" class="style1"><?php echo $info['titulo']; ?></h2>
                             <p id="text13" class="style2" style="font-size: 20px"><?php echo $info['subtitulo']; ?></p>
                         </div>
@@ -90,23 +93,25 @@ include_once '../../icon/network.php';
                             </svg><span class="label">Arrow Down</span></a></li>
                 </ul>
                 <hr id="divider02" class="style1 full" />
-                <div id="container02" data-scroll-id="start" data-scroll-behavior="bottom" data-scroll-offset="0" data-scroll-speed="3" data-scroll-invisible="1" class="style1 container default">
+                <div id="container02" data-scroll-id="start" data-scroll-behavior="bottom" data-scroll-offset="0"
+                    data-scroll-speed="3" data-scroll-invisible="1" class="style1 container default">
                     <div class="wrapper">
                         <div class="inner" data-onvisible-trigger="1">
-                            <h2 id="text14" class="style3" data-scroll-id="start" data-scroll-behavior="center" data-scroll-offset="0" data-scroll-speed="3" data-scroll-invisible="1">Meus Projetos
+                            <h2 id="text14" class="style3" data-scroll-id="start" data-scroll-behavior="center"
+                                data-scroll-offset="0" data-scroll-speed="3" data-scroll-invisible="1">Meus Projetos
                             </h2>
                             <ul id="buttons04" class="style1 buttons">
                                 <?php
                                 foreach ($projects as $project) {
                                 ?>
-                                    <li>
-                                        <a href="<?php echo $project['url_project'] ?>" class="button n01">
-                                            <svg>
-                                                <use xlink:href="#icon-49c7b76f0edfabe10e324ba1ac396f84"></use>
-                                            </svg>
-                                            <span class="label"><?php echo $project['project_name'] ?></span>
-                                        </a>
-                                    </li>
+                                <li>
+                                    <a href="<?php echo $project['url_project'] ?>" class="button n01">
+                                        <svg>
+                                            <use xlink:href="#icon-49c7b76f0edfabe10e324ba1ac396f84"></use>
+                                        </svg>
+                                        <span class="label"><?php echo $project['project_name'] ?></span>
+                                    </a>
+                                </li>
                                 <?php } ?>
                             </ul>
                             <p id="text01" class="style2">Tecnologias usadas em alguns projetos:</p>
@@ -118,11 +123,12 @@ include_once '../../icon/network.php';
                     <div class="inner" data-onvisible-trigger="1">
                         <ul id="icons01" class="style1 icons">
                             <?php foreach ($skills as $skill) { ?>
-                                <li>
-                                    <div id="image02" class="style2 image">
-                                        <img src="<?php echo '../../' . $skill['skill']; ?>" alt="ReactJS" widht="100px" height="100px" />
-                                    </div>
-                                </li>
+                            <li>
+                                <div id="image02" class="style2 image">
+                                    <img src="<?php echo '../../' . $skill['skill']; ?>" alt="ReactJS" widht="100px"
+                                        height="100px" />
+                                </div>
+                            </li>
                             <?php } ?>
                         </ul>
 
@@ -137,11 +143,13 @@ include_once '../../icon/network.php';
                             <ul id="icons01" class="style1 icons" style="padding: 25%;">
                                 <?php foreach ($others as $other) { ?>
 
-                                    <li>
-                                        <a href="<?php echo $other['banner_url'] ?>">
-                                            <img name="banner" id="banner" src="<?php echo '../../' . $other['banner']; ?>" alt="Palestra 'Testes com PHPUnit: o básico sobre TDD'" widht="200px" height="300px" />
-                                        </a>
-                                    </li>
+                                <li>
+                                    <a href="<?php echo $other['banner_url'] ?>">
+                                        <img name="banner" id="banner" src="<?php echo '../../' . $other['banner']; ?>"
+                                            alt="Palestra 'Testes com PHPUnit: o básico sobre TDD'" widht="200px"
+                                            height="300px" />
+                                    </a>
+                                </li>
                                 <?php } ?>
                             </ul>
                             <br>
@@ -157,10 +165,12 @@ include_once '../../icon/network.php';
                             <h2 id="text07" class="style3">Minhas
                                 redes</h2>
                             <ul id="icons01" class="style1 icons">
-                                <li><a class="n01" href="linkedin.com/in/<?php echo $social['linkedin']; ?>" aria-label="LinkedIn"><svg>
+                                <li><a class="n01" href="linkedin.com/in/<?php echo $social['linkedin']; ?>"
+                                        aria-label="LinkedIn"><svg>
                                             <use xlink:href="#icon-bf393d6ea48a4e69e1ed58a3563b94a5"></use>
                                         </svg><span class="label">LinkedIn</span></a></li>
-                                <li><a class="n02" href="https://github.com/<?php echo $social['github']; ?>" aria-label="GitHub"><svg>
+                                <li><a class="n02" href="https://github.com/<?php echo $social['github']; ?>"
+                                        aria-label="GitHub"><svg>
                                             <use xlink:href="#icon-8c4b37645de3c276d895d87df51ba614"></use>
                                         </svg><span class="label">GitHub</span></a></li>
                             </ul>
