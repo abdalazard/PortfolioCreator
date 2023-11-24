ed9a5fdc82730f62899d4652dca33e0fc37dd42f
abdalazard <abdalazard@gmail.com>
2023-11-24 03:03:04 -0300

    corrige env

--- db/EnvManager.php
+++ db/EnvManager.php
@@ -3,7 +3,7 @@
 class EnvManager {
     public function __construct()
     {
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv = Dotenv\Dotenv::createImmutable('../../');
         $dotenv->load();
     }
 }
