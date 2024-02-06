e0123839844e53e5a59eb1e969e88c390aff32bd
Vinicius <abdalazard@gmail.com>
2024-02-06 17:38:05 -0300

    corrige ordem de query

--- db/database.sql
+++ db/database.sql
@@ -96,17 +96,6 @@ CREATE TABLE `status` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-DROP TABLE IF EXISTS `template`;
-CREATE TABLE `template` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `creator_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `creator_id` (`creator_id`),
-  CONSTRAINT `template_ibfk_1` FOREIGN KEY (`creator_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
 DROP TABLE IF EXISTS `template_user`;
 CREATE TABLE `template_user` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -119,6 +108,15 @@ CREATE TABLE `template_user` (
   CONSTRAINT `template_user_ibfk_2` FOREIGN KEY (`template_id`) REFERENCES `template` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+DROP TABLE IF EXISTS `template`;
+CREATE TABLE `template` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `creator_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `creator_id` (`creator_id`),
+  CONSTRAINT `template_ibfk_1` FOREIGN KEY (`creator_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
